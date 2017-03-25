aef9e521a99e490fdc1774f6407cb9cdd41bc6c9
Rajat Nair <rajatnair@outlook.com>
2017-03-25 21:59:50 +0530

    Updated samba and deluge

--- samba/install.samba.sh
+++ samba/install.samba.sh
@@ -14,28 +14,56 @@ sudo tee /etc/samba/smb.conf <<-'EOF'
 workgroup = CARB0N
 server string = Odroid Server %v
 netbios name = odroid64
-security = user
-map to guest = bad user
 dns proxy = no
-name resolve order = bcast host
+log file = /var/log/samba/log.%m
+max log size = 1000
+syslog only = no
+syslog = 0
+panic action = /usr/share/samba/panic-action %d
+security = user
+encrypt passwords = true
+passdb backend = tdbsam
+obey pam restrictions = yes
+unix password sync = yes
+passwd program = /usr/bin/passwd %u
+passwd chat = *Enter\snew\s*\spassword:* %n\n *Retype\snew\s*\spassword:* %n\n *password\supdated\ssuccessfully* .
+pam password change = yes
+map to guest = bad user
+load printers = no
 
 #============================ Share Definitions ============================== 
 
+[Backups]
+comment = Backups
+path = /mnt/SeedboxIII/Backups
+browseable = yes
+create mask = 0775
+directory mask = 0775
+valid users = odroid
+writeable = yes
+
 [Carbon]
 path = /mnt/Carbon/
-browsable =yes
-writable = yes
-guest ok = yes
-read only = no
-#force user = nobody
+browseable = yes
+create mask = 0775
+directory mask = 0775
+valid users = odroid
+writeable = yes
 
 [Seedbox]
-path = [Carbon]
 path = /mnt/SeedboxIII/odroid-torrentbox
-browsable =yes
-writable = yes
+browseable = yes
+create mask = 0775
+directory mask = 0775
+valid users = odroid
+writeable = yes
+
+[Media]
+path = /mnt/SeedboxIII/Media
+browsable = yes
+writable = no
 guest ok = yes
-read only = no
+read only = yes
 EOF
 
 
